fc40991e839f9ce29ec507250edfa1af61fcde81
Павел Танков <pasha.esca1a@gmail.com>
2025-08-23 05:43:01 +0300

    feat(auto.vehicle): добавил кеш в индексный ответ

--- dataloft/Auto/Vehicle/Controllers/Controller.php
+++ dataloft/Auto/Vehicle/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace Dataloft\Auto\Vehicle\Controllers;
 
 use Dataloft\Auto\Vehicle\Models\Vehicle;
+use Dataloft\Auto\Vehicle\Requests\CreateVehicleRequest;
 use Dataloft\Auto\Vehicle\Requests\IndexVehicleRequest;
 use Dataloft\Auto\Vehicle\Resources\VehicleResource;
 use App\Http\Controllers\Controller as BaseController;
@@ -10,14 +11,23 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class Controller extends BaseController
 {
+    /**
+     */
     public function index(IndexVehicleRequest $request): AnonymousResourceCollection
     {
-        return VehicleResource::collection(
-            Vehicle::query()->with('getCarModel.getBrand')->paginate($request->limit)
-        );
+        $vehicleResource = cache('vehicle-resource-index');
+
+        if (!$vehicleResource) {
+            $vehicleResource = cache('vehicle-resource-index') ?? VehicleResource::collection(
+                Vehicle::query()->with('getCarModel.getBrand')->paginate($request->limit)
+            );
+            cache()->add('vehicle-resource-index', $vehicleResource);
+        }
+
+        return $vehicleResource;
     }
 
-    public function create()
+    public function create(CreateVehicleRequest $request)
     {
     }
 
